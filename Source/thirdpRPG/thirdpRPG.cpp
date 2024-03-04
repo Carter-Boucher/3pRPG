1b05aed3c17f8550818573d177d34d43c7679860
Carter Boucher <carterboucher85@gmail.com>
2024-03-04 19:53:26 -0400

    setup

---
+++ Source/thirdpRPG/thirdpRPG.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "thirdpRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, thirdpRPG, "thirdpRPG" );
+ 
---
+++ Source/thirdpRPG/thirdpRPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "thirdpRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AthirdpRPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AthirdpRPGGameMode();
+};
+
+
+
